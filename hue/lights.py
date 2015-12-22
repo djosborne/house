0d6f0812033987c1fb1c73ab0b2d726592cbbc7e
Dan Osborne <djosborne10@gmail.com>
2015-12-22 01:37:27 -0500

    Added toggling

--- hue/lights.py
+++ hue/lights.py
@@ -1,8 +1,20 @@
 from phue import Bridge
 b = Bridge('192.168.1.65')
 
+dans_lights = ['Dans Window', 'Dans Bookcase', 'Dans Globe', 'Dans Desk']
+
 def dans_lights_on():
-	b.set_light(['Dans Window', 'Dans Bookcase', 'Dans Globe', 'Dans Desk'], 'on', True)
+	b.set_light(dans_lights, 'on', True)
+
+def dans_lights_off():
+	b.set_light(dans_lights, 'on', False)
+
+def any_light_is_on():
+	for light in dans_lights:
+		if b.get_light('Dans Window')['state']['on']:
+			return True
+	return False
+
 
 from scapy.all import *
 
@@ -12,7 +24,10 @@ def arp_display(pkt):
       if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
         if pkt[ARP].hwsrc == 'a0:02:dc:d3:42:4d': # ON - dans room
           print 'Dans Room - On'
-          dans_lights_on()
+          if any_light_is_on():
+            dans_lights_off()
+          else:
+            dans_lights_on()
         else:
           print "Unknown Arp: " + pkt[ARP].hwsrc
   except IndexError:
