0ba347cb7a8ec8d6962184a5275dd37ab674220b
Dan Osborne <djosborne10@gmail.com>
2016-01-05 01:07:37 -0500

    Even cleaner!

--- hue/lights.py
+++ hue/lights.py
@@ -4,7 +4,8 @@ from phue import Bridge
 b = Bridge('192.168.1.65')
 
 class Room(object):
-    def __init__(self, lights):
+    def __init__(self, nickname, lights):
+        self.nickname = nickname
         self.lights = lights
 
     def turn_lights_off(self):
@@ -19,13 +20,18 @@ class Room(object):
                 return True
         return False
 
-dans_room = Room(['Dans room 1',
+    def __str__(self):
+        return self.nickname
+
+dans_room = Room('Dans Room',
+                 ['Dans room 1',
                   'Dans Window',
                   'Dans Bookcase',
                   'Dans Globe',
                   'Dans Desk'])
 
-living_room = Room(['Fireplace Strips',
+living_room = Room('Living Room',
+                   ['Fireplace Strips',
                     'Clock 2',
                     'TV Strip Left',
                     'TV Left',
@@ -44,25 +50,22 @@ living_room = Room(['Fireplace Strips',
                     'Dining Room 2',
                     'Go'])
 
+room_by_mac = {'a0:02:dc:d3:42:4d': dans_room,
+               '74:75:48:dc:2b:2f': living_room}
 
 def arp_display(pkt):
     try:
         if pkt[ARP].op == 1: #who-has (request)
             if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
-                # Dans Room
-                if pkt[ARP].hwsrc == 'a0:02:dc:d3:42:4d':
-                    print 'Dans Room'
-                    if dans_room.any_light_is_on():
-                        dans_room.turn_lights_off()
+                mac = pkt[ARP].hwsrc
+                try:
+                    room = room_by_mac[mac]
+                    print room
+                    if room.any_light_is_on():
+                        room.turn_lights_off()
                     else:
-                        dans_room.turn_lights_on()
-                elif pkt[ARP].hwsrc == '74:75:48:dc:2b:2f':
-                    print 'Living Room On/Off'
-                    if living_room.any_light_is_on():
-                        living_room.turn_lights_off()
-                    else:
-                        living_room.turn_lights_on()
-                else:
+                        room.turn_lights_on()
+                except KeyError:
                   print "Unknown Arp: " + pkt[ARP].hwsrc
     except IndexError:
         pass
