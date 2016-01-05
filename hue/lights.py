3eca262d0158772b4f57cac7efb5591cc97153e8
Dan Osborne <djosborne10@gmail.com>
2016-01-05 02:39:04 -0500

    Added mood rooms

--- hue/lights.py
+++ hue/lights.py
@@ -1,5 +1,6 @@
 from scapy.all import *
 from phue import Bridge
+from collections import namedtuple
 
 b = Bridge('192.168.1.65')
 
@@ -23,35 +24,112 @@ class Room(object):
     def __str__(self):
         return self.nickname
 
-dans_room = Room('Dans Room',
-                 ['Dans room 1',
-                  'Dans Window',
-                  'Dans Bookcase',
-                  'Dans Globe',
-                  'Dans Desk'])
 
-living_room = Room('Living Room',
-                   ['Fireplace Strips',
-                    'Clock 2',
-                    'TV Strip Left',
-                    'TV Left',
-                    'Desk',
-                    'Paintings',
-                    'Desk Strips',
-                    'Cats',
-                    'Dining Room 1', # May be unnecessary
-                    'Clock 1',
-                    'Clock 3',
-                    'TV Strips Right',
-                    'Dining Room 3',
-                    'TV Right 1',
-                    'iMac',
-                    'TV Right 2',
-                    'Dining Room 2',
-                    'Go'])
+Light = namedtuple('Light', ['id', 'default_mode', 'backup_mode'])
+"""
+:param default_mode: Represents the settings the light should switch to if
+it (or any other light) is not in its default_mode. 
+Usually, this is "lights off". But if we're doing a mood switch,
+this is the pretty mode.
+
+:param backup_mode: Respresents the settings the light should switch to if
+it *and* all other lights are currently in default_mode.
+"""
+
+
+class MoodRoom(object):
+    def __init__(self, nickname, lights):
+        self.nickname = nickname
+        self.lights = lights
+
+    def not_in_default_mode(self):
+        """
+        return True if not in default mode.
+        """
+        for light in self.lights:
+            hue_light = b.get_light(light.id)
+            for option, value in light.default_mode.iteritems():
+                if hue_light['state'][option] != value:
+                    return True
+        return False
+
+    def activate_default_mode(self):
+        for light in self.lights:
+            b.set_light(light.id, light.default_mode)
+
+    def activate_backup_mode(self):
+        for light in self.lights:
+            b.set_light(light.id, light.backup_mode)
+
+
+dans_room = MoodRoom('Dans Room',
+                   [# TV Area
+                    Light('Dans room 1', {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Dans Window', {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Dans Bookcase', {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Dans Globe', {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Dans Desk', {'on': False}, {'on': True, 'bri': 254})])
+
+
+living_room_moody = MoodRoom('Living Room - Moody',
+                   [# TV Area
+                    Light('Fireplace Strips', {'on': True, 'bri': 254}, {'on': True}),
+                    Light('Cats',             {'on': True, 'bri': 254}, {'on': True}),
+                    Light('TV Left',          {'on': True, 'bri': 254}, {'on': True}),
+                    Light('TV Strip Left',    {'on': True, 'bri': 254}, {'on': True}),
+                    Light('TV Right 1',       {'on': True, 'bri': 254}, {'on': True}),
+                    Light('TV Right 2',       {'on': True, 'bri': 254}, {'on': True}),
+                    Light('TV Strips Right',  {'on': True, 'bri': 254}, {'on': True}),
+
+                    # Left wall
+                    Light('Paintings', {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Clock 1',   {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Clock 2',   {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Clock 3',   {'on': False}, {'on': True, 'bri': 254}),
+                    Light('iMac',      {'on': False}, {'on': True, 'bri': 254}),
+
+                    # Dining Room
+                    Light('Dining Room 1', {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Dining Room 2', {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Dining Room 3', {'on': False}, {'on': True, 'bri': 254}),
+
+                    # Tyler's desk
+                    Light('Desk Strips', {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Desk',        {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Go',         {'on': False}, {'on': True, 'bri': 254})])
+
+
+living_room = MoodRoom('Living Room',
+                   [# TV Area
+                    Light('Fireplace Strips', {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Cats',             {'on': False}, {'on': True, 'bri': 254}),
+                    Light('TV Left',          {'on': False}, {'on': True, 'bri': 254}),
+                    Light('TV Strip Left',    {'on': False}, {'on': True, 'bri': 254}),
+                    Light('TV Right 1',       {'on': False}, {'on': True, 'bri': 254}),
+                    Light('TV Right 2',       {'on': False}, {'on': True, 'bri': 254}),
+                    Light('TV Strips Right',  {'on': False}, {'on': True, 'bri': 254}),
+
+                    # Left wall
+                    Light('Paintings', {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Clock 1',   {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Clock 2',   {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Clock 3',   {'on': False}, {'on': True, 'bri': 254}),
+                    Light('iMac',      {'on': False}, {'on': True, 'bri': 254}),
+
+                    # Dining Room
+                    Light('Dining Room 1', {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Dining Room 2', {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Dining Room 3', {'on': False}, {'on': True, 'bri': 254}),
+
+                    # Tyler's desk
+                    Light('Desk Strips', {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Desk',        {'on': False}, {'on': True, 'bri': 254}),
+                    Light('Go',         {'on': False}, {'on': True, 'bri': 254})])
+
 
 room_by_mac = {'a0:02:dc:d3:42:4d': dans_room,
-               '74:75:48:dc:2b:2f': living_room}
+               '74:75:48:dc:2b:2f': living_room,
+               '00:bb:3a:8c:53:14': living_room_moody}
 
 def arp_display(pkt):
     try:
@@ -61,10 +139,10 @@ def arp_display(pkt):
                 try:
                     room = room_by_mac[mac]
                     print room
-                    if room.any_light_is_on():
-                        room.turn_lights_off()
+                    if room.not_in_default_mode():
+                        room.activate_default_mode()
                     else:
-                        room.turn_lights_on()
+                        room.activate_backup_mode()
                 except KeyError:
                   print "Unknown Arp: " + pkt[ARP].hwsrc
     except IndexError:
